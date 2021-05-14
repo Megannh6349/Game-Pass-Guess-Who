3dc3795e90ca671ace5d4748cfa560e376ddbd88
Megan H <megannh6349@gmail.com>
2021-05-14 18:19:09 -0400

    added descriptions for eventual shift to jquery

---
+++ js/choices.js
@@ -0,0 +1,102 @@
+const choices = [
+    {
+        name: 'Tidus',
+        game: 'Final Fantasy X',
+        description: ''
+    },
+    {
+        name: 'Crypto-137',
+        game: 'Destroy All Humans!',
+        description: ''
+    },
+    {
+        name: 'Raz',
+        game: 'Psychonauts',
+        description: ''
+    },
+    {
+        name: 'Isaac Clarke',
+        game: 'Dead Space',
+        description: ''
+    },
+    {
+        name: 'Master Chief',
+        game: 'Halo',
+        description: ''
+    },
+    {
+        name: 'Commander Shepard',
+        game: 'Mass Effect',
+        description: ''
+    },
+    {
+        name: 'Kait Diaz',
+        game: 'Gears 5',
+        description: ''
+    },
+    {
+        name: 'Ori',
+        game: 'Ori',
+        description: ''
+    },
+    {
+        name: 'Marianne',
+        game: 'The Medium',
+        description: ''
+    },
+    {
+        name: 'Talion',
+        game: 'Middle Earth:  Shadow of War',
+        description: ''
+    },
+    {
+        name: 'Lucky',
+        game: 'Super Luckys Tail',
+        description: ''
+    },
+    {
+        name: 'Jesse Faden',
+        game: 'Control',
+        description: ''
+    },
+    {
+        name: 'Celeste',
+        game: 'Celeste',
+        description: ''
+    },
+    {
+        name: 'Cayde-6',
+        game: 'Destiny 2',
+        description: ''
+    },
+    {
+        name: 'Alex',
+        game: 'Minecraft',
+        description: ''
+    },
+    {
+        name: 'Batman',
+        game: 'Injustice 2',
+        description: ''
+    },
+    {
+        name: 'Goat',
+        game: 'Goat Simulator',
+        description: ''
+    },
+    {
+        name: '2B',
+        game: 'Nier: Automata',
+        description: ''
+    },
+    {
+        name: 'The Beheaded',
+        game: 'Dead Cells',
+        description: ''
+    },
+    {
+        name: 'Will',
+        game: 'Moonlighter',
+        description: ''
+    },
+];
